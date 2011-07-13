8c2f26573d2988a86d553a042059d46c656f5bc7
gedisu@gmail.com <gedisu@gmail.com@5ac02f81-0a28-52a4-0b05-b9d9838f11dd>
2011-07-13 13:02:39 +0000

    Antras

---
+++ trunk/BlogeriuNaujienos/src/net/gedzis/blogeriunaujienos/BlogeriuNaujienos.java
@@ -0,0 +1,13 @@
+package net.gedzis.blogeriunaujienos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BlogeriuNaujienos extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
